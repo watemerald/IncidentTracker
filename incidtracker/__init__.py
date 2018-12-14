997960d86b2202ba7cf3d1f1530c16d20b2c0b95
Andrii Sherman <watemerald@gmail.com>
2018-12-14 12:38:53 -0500

    Trying to fix db issues

--- incidtracker/__init__.py
+++ incidtracker/__init__.py
@@ -8,6 +8,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:adelphi@postgres:5432/'
 db = SQLAlchemy(app)
+db.create_all()
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
