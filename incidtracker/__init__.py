1509ba2a2d683598d483b8bcab65761a87df32fc
Andrii Sherman <watemerald@gmail.com>
2018-12-14 13:27:02 -0500

    DB Migrations

--- incidtracker/__init__.py
+++ incidtracker/__init__.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:adelphi@postgres:5432/'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:adelphi@postgres:5432/'
 db = SQLAlchemy(app)
-db.create_all()
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
